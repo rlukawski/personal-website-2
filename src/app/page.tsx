80b728e4b39018450bd2499646f5471a04f20e01
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-26 05:02:56 +0100

    Set background page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Container from "@mui/material/Container";
-import { grey } from "@mui/material/colors";
 import { ResponsiveAppBar } from "@/components/ResponsiveAppBar";
 import { HeaderSection } from "@/components/HeaderSection";
 import { AboutSection } from "@/components/AboutSection";
@@ -11,7 +10,7 @@ import { ContactSection } from "@/components/ContactSection";
 
 export default function Page() {
   return (
-    <Container maxWidth="xl" sx={{ backgroundColor: grey[100], paddingX: 0 }}>
+    <Container maxWidth="xl" sx={{ paddingX: 0 }}>
       <ResponsiveAppBar />
       <HeaderSection />
       <AboutSection />
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,9 +1,14 @@
 "use client";
 
 import { createTheme } from "@mui/material/styles";
+import { grey } from "@mui/material/colors";
 
 const theme = createTheme({
-  // You can customize your theme here
+  palette: {
+    background: {
+      default: grey[100],
+    },
+  },
 });
 
 export default theme;
