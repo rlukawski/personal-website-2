f4cd33beed7e163dc985e11a0988cd0823ab3768
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-28 08:07:41 +0100

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,36 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://lukawski.eu';
   
+  const alternates = {
+    languages: {
+      en: `${baseUrl}/en`,
+      pl: `${baseUrl}/pl`,
+    },
+  };
+
   return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+      alternates,
+    },
     {
       url: `${baseUrl}/en`,
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 1,
+      priority: 0.9,
+      alternates,
     },
     {
       url: `${baseUrl}/pl`,
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 1,
+      priority: 0.9,
+      alternates,
     },
   ];
 }
